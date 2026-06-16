bc279d3e2d57eb70627d7f708a212b2c8b2be90f
Lars-Olav Vågene <42863501+LarsV123@users.noreply.github.com>
2026-06-16 10:53:49 +0200

    fix: Remove unnecessary PMD suppressions

--- nva-language/src/main/java/no/unit/nva/language/LanguageMapper.java
+++ nva-language/src/main/java/no/unit/nva/language/LanguageMapper.java
@@ -15,7 +15,6 @@ import static no.unit.nva.language.LanguageConstants.NORWEGIAN;
 import static no.unit.nva.language.LanguageConstants.UNDEFINED_LANGUAGE;
 
 public final class LanguageMapper {
-    private static final List<LanguageDescription> languages = ALL_LANGUAGES;
     public static final String EMPTY_STRING = "";
 
     @JacocoGenerated
@@ -23,37 +22,37 @@ public final class LanguageMapper {
     }
 
     public static Language getLanguageByIso6391Code(String code) {
-        return (Language) mappedValue(languages.stream().filter(language -> convertString(code)
+        return (Language) mappedValue(ALL_LANGUAGES.stream().filter(language -> convertString(code)
                 .equalsIgnoreCase(language.getIso6391Code())));
     }
 
     public static Language getLanguageByIso6392Code(String code) {
-        return (Language) mappedValue(languages.stream().filter(language -> language.getIso6392Code().stream()
+        return (Language) mappedValue(ALL_LANGUAGES.stream().filter(language -> language.getIso6392Code().stream()
                 .anyMatch(lang -> convertString(code).equalsIgnoreCase(lang))));
     }
 
     public static Language getLanguageByIso6393Code(String code) {
-        return (Language) mappedValue(languages.stream().filter(language -> convertString(code)
+        return (Language) mappedValue(ALL_LANGUAGES.stream().filter(language -> convertString(code)
                 .equalsIgnoreCase(language.getIso6393Code())));
     }
 
     public static LanguageDescription getLanguageByEnglishName(String name) {
-        return mappedValue(languages.stream().filter(language -> convertString(name)
+        return mappedValue(ALL_LANGUAGES.stream().filter(language -> convertString(name)
                 .equalsIgnoreCase(language.getEng())));
     }
 
     public static LanguageDescription getLanguageByBokmaalName(String name) {
-        return mappedValue(languages.stream().filter(language -> convertString(name)
+        return mappedValue(ALL_LANGUAGES.stream().filter(language -> convertString(name)
                 .equalsIgnoreCase(language.getNob())));
     }
 
     public static LanguageDescription getLanguageByNynorskName(String name) {
-        return mappedValue(languages.stream().filter(language -> convertString(name)
+        return mappedValue(ALL_LANGUAGES.stream().filter(language -> convertString(name)
                 .equalsIgnoreCase(language.getNno())));
     }
 
     public static LanguageDescription getLanguageBySamiName(String name) {
-        return mappedValue(languages.stream().filter(language -> convertString(name)
+        return mappedValue(ALL_LANGUAGES.stream().filter(language -> convertString(name)
                 .equalsIgnoreCase(language.getSme())));
     }
 
@@ -63,12 +62,12 @@ public final class LanguageMapper {
     }
 
     public static LanguageGroup getLanguageByIso6395Code(String iso6395Code) {
-        return (LanguageGroup) mappedValue(languages.stream()
+        return (LanguageGroup) mappedValue(ALL_LANGUAGES.stream()
                  .filter(language -> iso6395Code.equals(language.getIso6395Code())));
     }
 
     public static LanguageDescription getLanguageByPotentialIsoCode(String potentialIsoCode) {
-        return mappedValue(languages.stream().filter(language -> matchesCode(potentialIsoCode, language)));
+        return mappedValue(ALL_LANGUAGES.stream().filter(language -> matchesCode(potentialIsoCode, language)));
     }
 
     private static boolean matchesCode(String potentialIsoCode, LanguageDescription language) {
@@ -90,17 +89,11 @@ public final class LanguageMapper {
     }
 
     private static LanguageDescription replaceMiscellaneous(LanguageDescription language) {
-        if (MISCELLANEOUS.equals(language)) {
-            return MULTIPLE;
-        }
-        return language;
+        return MISCELLANEOUS.equals(language)? MULTIPLE : language;
     }
 
     private static LanguageDescription replaceNorwegian(LanguageDescription language) {
-        if (NORWEGIAN.equals(language)) {
-            return BOKMAAL;
-        }
-        return language;
+        return NORWEGIAN.equals(language)? BOKMAAL : language;
     }
 
     private static String convertString(String code) {
